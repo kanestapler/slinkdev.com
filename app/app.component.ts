25f3465ef5f8ed57e9941f8cd92afe2ee9116ada
kanestapler <kanestapler@gmail.com>
2016-06-09 18:15:52 -0500

    fixing git ignore

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'slinkdev-heading',
-  template: '<h1>Slinkdev</h1>'
+  template: '<h1>Slinkdev coming soon!</h1>'
 })
 export class AppComponent { }
